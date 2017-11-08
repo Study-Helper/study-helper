cfb5b3eb83751dbe109d23ba2e8ac8a17978dd23
Xico <fjm.cunha@campus.fct.unl.pt>
2017-11-08 14:18:33 +0000

    Fixed naming on the stylesheet properties

--- styles/styles.css.js
+++ styles/styles.css.js
@@ -2,52 +2,52 @@
 
 export const sidebar = {
   link: {
-    'textDecoration': 'none'
+    textDecoration: 'none'
   },
   menuItem: {
-    'height': '60px'
+    height: '60px'
   },
   component: {
-    'paddingLeft': '179px'
+    paddingLeft: '179px'
   }
 };
 
 export const appbar = {
   barLayout: {
-    'width': '663px',
-    'height': '60px',
-    'marginTop': '-8px'
+    width: '663px',
+    height: '60px',
+    marginTop: '-8px'
   }
 };
 
 export const taskDescription = {
   description: {
-    'fontFamily': 'Roboto',
-    'marginTop': '-20px',
-    'paddingLeft': '73px'
+    fontFamily: 'Roboto',
+    marginTop: '-20px',
+    paddingLeft: '73px'
   },
   icon: {
-    'paddingLeft': '20px',
-    'paddingTop': '7px'
+    paddingLeft: '20px',
+    paddingTop: '7px'
   }
 };
 
 export const taskList = {
   list: {
-    'width': '665px'
+    width: '665px'
   },
   avatar: {
-    'marginTop': '3px'
+    marginTop: '3px'
   },
   iconButton: {
-    'marginTop': '-7px',
-    'float': 'right'
+    marginTop: '-7px',
+    float: 'right'
   }
 };
 
 export const moreOptions = {
   icon: {
-    'float':'right',
-    'marginTop': '-8px'
+    float:'right',
+    marginTop: '-8px'
   }
 };
